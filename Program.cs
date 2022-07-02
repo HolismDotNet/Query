da1bf5f30f229df20aa457f14128214726472264
Saeed Nemati <sn.1361@gmail.com>
2022-07-02 17:27:25 +0430

    committed by script

--- Program.cs
+++ Program.cs
@@ -1,10 +1,11 @@
 ﻿var connection = InfraConfig.GetConnectionString("Accounts");
 var masterConnection = Regex.Replace(connection, @"database=.*", "");
+masterConnection = masterConnection + "; Allow User Variables=true;";
 
 if (args.Length == 1 && args[0] == "DeleteData")
 {
     Logger.LogInfo($"Deleting all the data ...");
-    for (var i = 0; i < 5; i++)
+    for (var i = 0; i < 3; i++)
     {
         DataDeleter.MasterConnection = masterConnection;
         new DataDeleter().DeleteData();
@@ -13,10 +14,20 @@ if (args.Length == 1 && args[0] == "DeleteData")
 }
 else 
 {
-    foreach (var queryFile in args)
+    for (var i = 0; i < 3; i++)
     {
-        Logger.LogInfo($"Running {queryFile} ...");
-        Database.Open(masterConnection).Run(File.ReadAllText(queryFile));
-        Logger.LogSuccess($"Ran {queryFile}");
+        foreach (var queryFile in args)
+        {
+            try
+            {
+                Logger.LogInfo($"Running {queryFile} ...");
+                Database.Open(masterConnection).Run(File.ReadAllText(queryFile));
+                Logger.LogSuccess($"Ran {queryFile}");
+            }
+            catch (Exception ex)
+            {
+                Logger.LogException(ex);
+            }
+        }
     }
 }
